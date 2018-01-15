acdd841aefb02802a2f9bf7484fb015d0c5a2abf
yoshzawa <t.yoshizawa@jc-21.jp>
2018-01-15 10:48:07 +0900

    Matchクラスを変更した

--- SwissDraw/Match.cs
+++ SwissDraw/Match.cs
@@ -24,17 +24,67 @@ namespace SwissDraw
             result = 0;
         }
 
-        public Match[] MakeMatch(Dictionary<int, Person> persons, Match[] results)
+        public static Match[] MakeMatch(Dictionary<int, Person> persons, Match[] results)
         {
-            Match[] matches = new Match[1];
-            matches[0] = new Match (1,10);
+            // personsのkeyのみ取り出す
+            int[] keys = GetKeyArray(persons);
+            
+            // 配列を初期化する
+            int matchCount = keys.Length/2;
+
+            Match[] matches = MakeMatch1(matchCount, keys, results);
+
             return matches;
         }
-        public Match[] MergeMatch(Match[] results1, Match[] results2)
+
+        // 「対戦していない」「同じチームじゃない」「勝ち数が同じ」で対戦
+        public static Match[] MakeMatch1(int matchCount, int[] keys, Match[] results)
+        {
+            Match[] matches = new Match[matchCount];
+
+            for (int i = 0; i < matchCount; i++)
+            {
+                // 最小のくじ番号を取得する(使われていないこと)
+                int minKey = getMinimumKey(keys, matches);
+
+                // 対応する対戦相手のくじ番号を取得する（使われていない、対戦していない、同じチームじゃない、勝ち数が同じ）
+                int versusKey = getVersusKey1(minKey, keys, matches, results);
+
+                // versusKey<0なら、対戦相手は見つからなかったため、nullを返す
+                if (versusKey < 0)
+                {
+                    return null;
+                }
+                //対戦を保存する
+                matches[i] = new Match(minKey, versusKey);
+            }
+            return matches;
+        }
+
+        // 対応する対戦相手のくじ番号を取得する
+        //（使われていない、対戦していない、同じチームじゃない、勝ち数が同じ）
+        private static int getVersusKey1(int minKey, int[] keys, Match[] matches, Match[] results)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 最小のくじ番号を取得する(使われていないこと)
+        private static int getMinimumKey(int[] keys, Match[] matches)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 2つの配列をマージして1つにする
+        public static Match[] MergeMatch(Match[] results1, Match[] results2)
         {
             return results1;
         }
 
+        // personsのkeyを取り出して配列にする
+        public static int[] GetKeyArray(Dictionary<int, Person> persons)
+        {
+            return persons.Keys.ToArray();
+        }
 
     }
 
