dcffdf83fb9d3b9d46078bd01c8727e791c3f94e
yoshzawa <t.yoshizawa@jc-21.jp>
2018-01-17 17:22:29 +0900

    getVersusKey1を作りかけている

--- SwissDraw/Match.cs
+++ SwissDraw/Match.cs
@@ -32,23 +32,28 @@ namespace SwissDraw
             // 配列を初期化する
             int matchCount = keys.Length/2;
 
-            Match[] matches = MakeMatch1(matchCount, keys, results);
+            // 勝数でkeyを分割する
+            int[][] SplittedKeys = splitPersons(persons, results);
+
+
+            Match[] matches = MakeMatch1(matchCount, SplittedKeys,persons, results);
 
             return matches;
         }
 
+
         // 「対戦していない」「同じチームじゃない」「勝ち数が同じ」で対戦
-        public static Match[] MakeMatch1(int matchCount, int[] keys, Match[] results)
+        public static Match[] MakeMatch1(int matchCount, int[][] SplittedKeys,Dictionary<int, Person> persons,Match[] results)
         {
             Match[] matches = new Match[matchCount];
 
             for (int i = 0; i < matchCount; i++)
             {
                 // 最小のくじ番号を取得する(使われていないこと)
-                int minKey = GetMinimumKey(keys, matches);
+                int minKey = GetMinimumKey(SplittedKeys, matches);
 
                 // 対応する対戦相手のくじ番号を取得する（使われていない、対戦していない、同じチームじゃない、勝ち数が同じ）
-                int versusKey = getVersusKey1(minKey, keys, matches, results);
+                int versusKey = getVersusKey1(minKey, SplittedKeys, matches, persons,results);
 
                 // versusKey<0なら、対戦相手は見つからなかったため、nullを返す
                 if (versusKey < 0)
@@ -61,28 +66,127 @@ namespace SwissDraw
             return matches;
         }
 
+
+        // 勝ち数の多いほうから順番に、勝ち数が同じ人のIDを配列にまとめる
+        public static int[][] splitPersons(Dictionary<int, Person> persons, Match[] results)
+        {
+            Dictionary<int, int> winCountDic = new Dictionary<int, int>();
+            int[] keyArray = GetKeyArray(persons);
+            int maxWinCount = -1;
+            foreach(int i in keyArray)
+            {
+                int wCount = getWinCount(i, results);
+                winCountDic.Add(i, wCount);
+                if (maxWinCount< wCount)
+                {
+                    maxWinCount = wCount;
+                }
+            }
+            int[][] result = new int[maxWinCount + 1][];
+            for(int i=0; i<= maxWinCount; i++)
+            {
+                result[i] = makePersonArray(maxWinCount - i, winCountDic);
+            }
+            return result;
+        }
+
+        // winCountDicの中で、勝数がvの要素の配列を生成する
+        private static int[] makePersonArray( int v, Dictionary<int, int> winCountDic)
+        {
+            List<int> l
+                = new List<int>();
+            foreach (int i in winCountDic.Keys)
+            {
+                if (winCountDic[i] == v)
+                {
+                    l.Add(i);
+                }
+            }
+            return l.ToArray();
+        }
+
         // 対応する対戦相手のくじ番号を取得する
         //（使われていない、対戦していない、同じチームじゃない、勝ち数が同じ）
-        private static int getVersusKey1(int minKey, int[] keys, Match[] matches, Match[] results)
+        private static int getVersusKey1(int minKey, int[][] splittedKeys, Match[] matches, Dictionary<int, Person> persons, Match[] results)
         {
+            int i = 0;
+            bool flag = true;
+            while (flag == true)
+            {
+                if (splittedKeys[i].Contains(minKey) == true)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            while(i<= splittedKeys.Length)
+            {
+                // TODO : Write Logic
+
+            }
+
             throw new NotImplementedException();
         }
 
-        // 最小のくじ番号を取得する(使われていないこと)
-        public static int GetMinimumKey(int[] keys, Match[] matches)
+        // 指定されたkeyの勝ち数を調べる
+        private static int getWinCount(int key, Match[] results)
         {
-            int result = 100000;
-            foreach (int i in keys)
+            int winCount = 0;
+            foreach(Match result in results)
             {
-                if(containsKey(matches,i) == false)
+                if (checkWin(key, result) == true)
                 {
-                    if (i < result)
+                    winCount++;
+                }
+            }
+            return winCount;
+        }
+
+        private static Boolean checkWin(int key, Match result)
+        {
+            if (containsKey(result, key) == true)
+            {
+                int count = 0;
+                if (result.result == 1)
+                {
+                    if (result.person1 == key)
                     {
-                        result = i;
+                        return(true);
+                    }
+                }
+                else if (result.result == 2)
+                {
+                    if (result.person2 == key)
+                    {
+                        return (true);
                     }
                 }
             }
-            return result;
+            return false;
+        }
+
+
+        // 最小のくじ番号を取得する(使われていないこと)
+        private static int GetMinimumKey(int[][] splittedKeys, Match[] matches)
+        {
+            int minKey = 100000;
+            foreach(int[] a in splittedKeys)
+            {
+                foreach(int b in a)
+                {
+                    if (minKey > b)
+                    {
+                        if(containsKey(matches,b) == false)
+                        {
+                            minKey = b;
+                        }
+                    }
+                }
+            }
+            return minKey;
         }
 
         private static bool containsKey(Match[] matches, int i)
@@ -112,7 +216,8 @@ namespace SwissDraw
         // 2つの配列をマージして1つにする
         public static Match[] MergeMatch(Match[] results1, Match[] results2)
         {
-            return results1;
+            // TODO : Write Logic
+            throw new NotImplementedException();
         }
 
         // personsのkeyを取り出して配列にする
